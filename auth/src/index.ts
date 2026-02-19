22afb4afeb5172b558344f7c3cb21ee8df5f7988
yishai <yishai5555@gmail.com>
2026-02-19 17:59:04 +0200

    05-ingress-nginx

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,9 @@ const app = express();
 
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hi there!");
+});
 app.listen(3000, () => {
   console.log('Auth service is running on port 3000');
 });
